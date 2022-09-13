99905bfd3298a3e9ddad4e899d9b16c142740544
Abdullajon1997 <abdullajonibrohimov0@gmail.com>
2022-09-14 00:26:10 +0300

    3-oy 1-dars

---
+++ main.js
@@ -0,0 +1,92 @@
+// ====================== Tiplar bo'limi =========================//
+
+// 1-Misol
+// function metr(sm)
+// {
+//     let M;
+//     M=sm/100;
+//     console.log(M + " metr");
+// }metr(100)
+
+// 2-misol
+// function tonna(M)
+// {
+//     let T;
+//     T=M/1000;
+//     console.log(T);
+// }tonna(100)
+
+// 3-misol
+// function chiziq(a,b)
+// {
+//     let kesma;
+//     kesma=a/b;
+//     console.log(kesma);
+    
+// }chiziq(60,5)
+
+// 4-misol
+// function xona(a)
+// {
+//     let yuzlik,onlik,birlik;
+//     onlik=Math.trunc(a/10);
+//     birlik=a%10;
+//     console.log(onlik + " o'nlik qismi");
+//     console.log(birlik + " birlik qismi");
+// }xona(58)
+
+// 5-misol
+// function raqam(a)
+// {
+//     let yigindi,onlik,birlik;
+//     onlik=Math.trunc(a/10);
+//     birlik=a%10;
+//     yigindi=onlik+birlik;
+//     console.log(onlik + " o'nlik");
+//     console.log(birlik + " birlik");
+//     console.log(yigindi + " Yig'\indi");
+// }raqam(98)
+
+// 6-misol
+// function raqam(a)
+// {
+//     let kopaytma,onlik,birlik;
+//     onlik=Math.trunc(a/10);
+//     birlik=a%10;
+//     kopaytma=onlik*birlik;
+//     console.log(onlik + " o'nlik");
+//     console.log(birlik + " birlik");
+//     console.log(kopaytma + " ko'paytma");
+// }raqam(98)
+
+// 7-misol
+// function yuzlar(a)
+// {
+//     let yuz;
+//     if (a>999)
+//     {
+//         yuz=Math.floor(a/100);
+//         console.log(yuz + " Yuzlik xona birliklari");
+//     }
+//     else
+//     {
+//         console.log("Sal o'ylabloq son kiritish kerak");
+//     }
+
+// }yuzlar(2803)
+
+// 8-misol
+// function soniya(a)
+// {
+//     let minut ,soat;
+//     minut=a/60;
+//     soat=minut/60
+//     console.log(minut + " Minut o'tdi");
+//     console.log(soat + " Soat o'tdi");
+// }soniya(7200)
+
+
+
+
+
+
